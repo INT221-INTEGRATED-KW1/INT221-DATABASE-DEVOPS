4716dc2b97d9582db67f21afc7ed60ad27266717
Phatcharadol Thangplub <43667929+m4pol@users.noreply.github.com>
2024-04-24 20:31:30 +0700

    lazy commit

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
-# not finish yet...
-mv $1 /home/sysadmin/group-compose/$2
+if [ $1 = "backend" ]; then
+        mv ./target/$2 /home/sysadmin/group-compose/backend/target/deploy.jar
+elif [ $1 = "frontend" ]; then
+        mv ./dist/* /home/sysadmin/group-conpose/frontend/dist/
+else
+    echo "backend [jar-file.jar] - move .jar file to deplot site."
+    echo "frontend - move "dist" to deploy site."
+fi
