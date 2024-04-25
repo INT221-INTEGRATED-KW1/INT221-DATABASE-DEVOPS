4ab3a6b1867035536f0eec1d72b36852d84a58a6
Phatcharadol Thangplub <43667929+m4pol@users.noreply.github.com>
2024-04-25 10:18:34 +0700

    delete deploy script

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-if [ $1 = "backend" ]; then
-        mv /home/monthon/INT221-BACKEND/target/kanbanborad-0.0.1-SNAPSHOT.jar /home/sysadmin/group-compose/backend/target/deploy.jar
-elif [ $1 = "frontend" ]; then
-        mv /home/chanachai/INT221-FRONTEND/dist/* /home/sysadmin/group-conpose/frontend/dist/
-else
-    echo "backend - move .jar file to deplot site."
-    echo "frontend - move "dist" to deploy site."
-fi
