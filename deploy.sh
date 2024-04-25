ec13b7e7c93cfe2c7bf7eeca72d87a4b17440a00
Phatcharadol Thangplub <43667929+m4pol@users.noreply.github.com>
2024-04-25 09:59:42 +0700

    Add files via upload

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 if [ $1 = "backend" ]; then
-        mv ./target/$2 /home/sysadmin/group-compose/backend/target/deploy.jar
+        mv /home/monthon/INT221-BACKEND/target/kanbanborad-0.0.1-SNAPSHOT.jar /home/sysadmin/group-compose/backend/target/deploy.jar
 elif [ $1 = "frontend" ]; then
-        mv ./dist/* /home/sysadmin/group-conpose/frontend/dist/
+        mv /home/chanachai/INT221-FRONTEND/dist/* /home/sysadmin/group-conpose/frontend/dist/
 else
-    echo "backend [jar-file.jar] - move .jar file to deplot site."
+    echo "backend - move .jar file to deplot site."
     echo "frontend - move "dist" to deploy site."
 fi
