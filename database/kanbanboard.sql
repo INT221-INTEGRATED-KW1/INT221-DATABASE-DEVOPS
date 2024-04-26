9437d7363ef9bfb200c6d0228e55e672aea51e6a
Phatcharadol Thangplub <43667929+m4pol@users.noreply.github.com>
2024-04-26 20:11:52 +0700

    fix db script

--- database/kanbanboard.sql
+++ database/kanbanboard.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE kanbanboard;
 
+SET GLOBAL time_zone = '+00:00';
+
 USE kanbanboard;
 
 DROP TABLE IF exists tasks;
@@ -8,10 +10,10 @@ CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
     description TEXT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-    assignees VARCHAR(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
+    assignees varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
     status ENUM('NO_STATUS', 'TODO', 'DOING', 'DONE') NOT NULL DEFAULT 'NO_STATUS',
-    createdOn DATETIME DEFAULT CURRENT_TIMESTAMP,
-    updatedOn DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE NOW(),
+    createdOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updatedOn TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE NOW(),
     CHECK (CHAR_LENGTH(description) <= 500),
     CHECK (title <> ''),
     CHECK (description <> ''),
@@ -20,16 +22,42 @@ CREATE TABLE tasks (
 
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 
-insert into tasks (title, description, assignees, status)
-values ("TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0", 
-'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5', 
-'Assignees1Assignees2Assignees3', "NO_STATUS");
+INSERT INTO tasks (title, description, assignees, status, createdOn, updatedOn)
+VALUES (
+    'TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0', 
+    'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5', 
+    'Assignees1Assignees2Assignees3', 
+    'NO_STATUS', 
+    '2024-04-22 09:00:00', 
+    '2024-04-22 09:00:00'
+);
 
-insert into tasks (title, description, assignees, status)
-values ("Repository", null, null, 'TODO');
+INSERT INTO tasks (title, description, assignees, status, createdOn, updatedOn)
+VALUES (
+    'Repository', 
+    NULL, 
+    NULL, 
+    'TODO', 
+    '2024-04-22 09:05:00', 
+    '2024-04-22 14:00:00'
+);
 
-insert into tasks (title, description, assignees, status)
-values ("ดาต้าเบส", 'ສ້າງຖານຂໍ້ມູນ', 'あなた、彼、彼女 (私ではありません)', "DOING");
+INSERT INTO tasks (title, description, assignees, status, createdOn, updatedOn)
+VALUES (
+    'ดาต้าเบส', 
+    'ສ້າງຖານຂໍ້ມູນ', 
+    'あなた、彼、彼女 (私ではありません)', 
+    'DOING', 
+    '2024-04-22 09:10:00', 
+    '2024-04-25 00:00:00'
+);
 
-insert into tasks (title, description, assignees, status)
-values ("_Infrastructure_", '_Setup containers_', 'ไก่งวง กับ เพนกวิน', "DONE");
+INSERT INTO tasks (title, description, assignees, status, createdOn, updatedOn)
+VALUES (
+    '     _Infrastructure_     ', 
+    '     _Setup containers_     ', 
+    'ไก่งวง กับ เพนกวิน', 
+    'DONE', 
+    '2024-04-22 09:15:00', 
+    '2024-04-22 10:00:00'
+);
