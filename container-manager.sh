0eec0f2a2d14e957ab69610c4cb89927a421aa5e
Phatcharadol Thangplub <43667929+m4pol@users.noreply.github.com>
2024-05-13 16:41:44 +0700

    update alot

--- container-manager.sh
+++ container-manager.sh
@@ -4,13 +4,16 @@ if [ $1 = "devops" ]; then
     if [ $2 = "build" ]; then
         docker compose up --build -d && docker compose ps -a && docker ps -a
     elif [ $2 = "clean" ]; then
-        docker compose stop database frontend backend && docker compose rm database frontend backend
+        docker compose stop database frontend backend tester && docker compose rm database frontend backend tester
         docker rmi -f $(docker images -aq)
     elif [ $2 = "deploy" ]; then
         if [ $3 = "backend" ]; then
             mv /home/monthon/INT221-BACKEND/target/kanbanborad-0.0.1-SNAPSHOT.jar /home/sysadmin/group-compose/backend/target/deploy.jar
         elif [ $3 = "frontend" ]; then
             mv /home/chanachai/INT221-FRONTEND/dist/ /home/sysadmin/group-compose/frontend/
+        elif [ $3 = "tester" ]; then
+            mkdir ./tester/src-fe
+            cp -r /home/chanachai/INT221-FRONTEND/* /home/sysadmin/group-compose/tester/src-fe/
         else
             echo "backend - move .jar file to dedplot site."
             echo "frontend - move "dist" to deploy site."
@@ -19,14 +22,5 @@ if [ $1 = "devops" ]; then
         echo "build - build container from docker compose."
         echo "clean - stop and delete current docker container in compose, which's require root access."
         echo "deploy <site> - deploy "fronted" or "backend", which's require root access."
-    fi
-elif [ $1 = "developer" ]; then
-    if [ $2 = "backend" ]; then
-        git clone https://github.com/INT221-INTEGRATED-KW1/INT221-BACKEND.git && sh ./INT221-BACKEND/mvnw clean package
-    elif [ $2 = "frontend" ]; then
-        git clone https://github.com/INT221-INTEGRATED-KW1/INT221-FRONTEND.git && cd ./INT221-FRONTEND && npm i && npm run build
-    else
-        echo "backend - build back-end project"
-        echo "frontend - build front-end project"
     fi
 fi
--- database/kanbanboard.sql
+++ database/kanbanboard.sql
@@ -15,7 +15,7 @@ CREATE TABLE status (
   status_id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL UNIQUE,
   description VARCHAR(500) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
-  color VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  color VARCHAR(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci,
   CHECK (name <> ''),
   CHECK (description <> ''),
   CHECK (color <> '')
@@ -45,3 +45,44 @@ INSERT INTO `status` (`status_id`, `name`, `description`,`color`) VALUES (1, 'No
 INSERT INTO `status` (`status_id`, `name`, `description`,`color`) VALUES (2, 'To Do', null,'orange');
 INSERT INTO `status` (`status_id`, `name`, `description`, `color`) VALUES (3, 'Doing', 'Being worked on', 'blue');
 INSERT INTO `status` (`status_id`, `name`, `description`, `color`) VALUES (4, 'Done', 'Finished','green');
+
+
+INSERT INTO tasks (title, description, assignees, status_id, createdOn, updatedOn)
+VALUES (
+    'TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0', 
+    'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5', 
+    'Assignees1Assignees2Assignees3', 
+    1, 
+    '2024-04-22 09:00:00', 
+    '2024-04-22 09:00:00'
+);
+
+INSERT INTO tasks (title, description, assignees, status_id, createdOn, updatedOn)
+VALUES (
+    'Repository', 
+    NULL, 
+    NULL, 
+    2,
+    '2024-04-22 09:05:00', 
+    '2024-04-22 14:00:00'
+);
+
+INSERT INTO tasks (title, description, assignees, status_id, createdOn, updatedOn)
+VALUES (
+    'ดาต้าเบส', 
+    'ສ້າງຖານຂໍ້ມູນ', 
+    'あなた、彼、彼女 (私ではありません)', 
+    3, 
+    '2024-04-22 09:10:00', 
+    '2024-04-25 00:00:00'
+);
+
+INSERT INTO tasks (title, description, assignees, status_id, createdOn, updatedOn)
+VALUES (
+    '_Infrastructure_ ', 
+    ' _Setup containers_', 
+    'ไก่งวง กับ เพนกวิน', 
+    4, 
+    '2024-04-22 09:15:00', 
+    '2024-04-22 10:00:00'
+);
