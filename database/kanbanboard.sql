c7f63cca02e156fe79900b82be757fbd98529eee
Phatcharadol Thangplub <43667929+m4pol@users.noreply.github.com>
2024-05-13 09:08:39 +0700

    update db

--- database/kanbanboard.sql
+++ database/kanbanboard.sql
@@ -1,4 +1,3 @@
-
 DROP DATABASE IF EXISTS kanbanboard;
 
 CREATE DATABASE kanbanboard;
@@ -42,47 +41,7 @@ CREATE TABLE tasks (
   CHECK (assignees <> '')
 );
 
-INSERT INTO `status` (`status_id`, `name`, `color`) VALUES (1, 'NO_STATUS', 'gray');
-INSERT INTO `status` (`status_id`, `name`, `color`) VALUES (2, 'TO_DO', 'orange');
-INSERT INTO `status` (`status_id`, `name`, `color`) VALUES (3, 'DOING', 'blue');
-INSERT INTO `status` (`status_id`, `name`, `color`) VALUES (4, 'DONE', 'green');
-
-INSERT INTO tasks (title, description, assignees, status_id, createdOn, updatedOn)
-VALUES (
-    'TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0', 
-    'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5', 
-    'Assignees1Assignees2Assignees3', 
-    1, 
-    '2024-04-22 09:00:00', 
-    '2024-04-22 09:00:00'
-);
-
-INSERT INTO tasks (title, description, assignees, status_id, createdOn, updatedOn)
-VALUES (
-    'Repository', 
-    NULL, 
-    NULL, 
-    2,
-    '2024-04-22 09:05:00', 
-    '2024-04-22 14:00:00'
-);
-
-INSERT INTO tasks (title, description, assignees, status_id, createdOn, updatedOn)
-VALUES (
-    'ดาต้าเบส', 
-    'ສ້າງຖານຂໍ້ມູນ', 
-    'あなた、彼、彼女 (私ではありません)', 
-    3, 
-    '2024-04-22 09:10:00', 
-    '2024-04-25 00:00:00'
-);
-
-INSERT INTO tasks (title, description, assignees, status_id, createdOn, updatedOn)
-VALUES (
-    '_Infrastructure_ ', 
-    ' _Setup containers_', 
-    'ไก่งวง กับ เพนกวิน', 
-    4, 
-    '2024-04-22 09:15:00', 
-    '2024-04-22 10:00:00'
-);
+INSERT INTO `status` (`status_id`, `name`, `description`,`color`) VALUES (1, 'No Status', 'The default status','gray');
+INSERT INTO `status` (`status_id`, `name`, `description`,`color`) VALUES (2, 'To Do', null,'orange');
+INSERT INTO `status` (`status_id`, `name`, `description`, `color`) VALUES (3, 'Doing', 'Being worked on', 'blue');
+INSERT INTO `status` (`status_id`, `name`, `description`, `color`) VALUES (4, 'Done', 'Finished','green');
